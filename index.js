1595d07eb4c60d05affb2d94ddfa1d43adb22279
SANAGO <sanago3130@gmail.com>
2022-09-17 13:03:30 +0200

    update

--- index.js
+++ index.js
@@ -20,31 +20,48 @@ console.log(
         color: 'blue',
         whitespaceBreak: true,
     }),
-    '\x1b[38;5;27mby SANAGO#0001\x1b[38;5;79m'
+    '\x1b[38;5;27mby SANAGO#0001'
 );
 
 //Application
-readline.question('Entrez un domaine : >', (query) => {
-    let url = `http://ip-api.com/json/${query}`;
 
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(`\x1b[35m> Query : \x1b[33m${data.query}`);
-            console.log(`\x1b[35m> Status : \x1b[33m${data.status}`);
-            console.log(`\x1b[35m> Country : \x1b[33m${data.country}`);
-            console.log(`\x1b[35m> Country Code : \x1b[33m${data.countryCode}`);
-            console.log(`\x1b[35m> Region : \x1b[33m${data.regionName}`);
-            console.log(`\x1b[35m> Region Code : \x1b[33m${data.region}`);
-            console.log(`\x1b[35m> City : \x1b[33m${data.city}`);
-            console.log(`\x1b[35m> Zip : \x1b[33m${data.zip}`);
-            console.log(`\x1b[35m> Time Zone : \x1b[33m${data.timezone}`);
-            console.log(`\x1b[35m> Latitude : \x1b[33m${data.lat}`);
-            console.log(`\x1b[35m> Longitude : \x1b[33m${data.lon}`);
-            console.log(`\x1b[35m> Isp : \x1b[33m${data.isp}`);
-            console.log(`\x1b[35m> Org : \x1b[33m${data.org}`);
-            console.log(`\x1b[35m> As : \x1b[33m${data.as}\x1b[32m`);
-        })
-        .catch((err) => console.log(`Erreur : ${err}`));
-    readline.close();
-});
+function app() {
+    readline.question('\x1b[38;5;79mEntrez un domaine : >', (query) => {
+        let url = `http://ip-api.com/json/${query}`;
+
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(`\x1b[35m> Query : \x1b[33m${data.query}`);
+                console.log(`\x1b[35m> Status : \x1b[33m${data.status}`);
+                console.log(`\x1b[35m> Country : \x1b[33m${data.country}`);
+                console.log(
+                    `\x1b[35m> Country Code : \x1b[33m${data.countryCode}`
+                );
+                console.log(`\x1b[35m> Region : \x1b[33m${data.regionName}`);
+                console.log(`\x1b[35m> Region Code : \x1b[33m${data.region}`);
+                console.log(`\x1b[35m> City : \x1b[33m${data.city}`);
+                console.log(`\x1b[35m> Zip : \x1b[33m${data.zip}`);
+                console.log(`\x1b[35m> Time Zone : \x1b[33m${data.timezone}`);
+                console.log(`\x1b[35m> Latitude : \x1b[33m${data.lat}`);
+                console.log(`\x1b[35m> Longitude : \x1b[33m${data.lon}`);
+                console.log(`\x1b[35m> Isp : \x1b[33m${data.isp}`);
+                console.log(`\x1b[35m> Org : \x1b[33m${data.org}`);
+                console.log(`\x1b[35m> As : \x1b[33m${data.as}\x1b[32m`);
+            })
+            .then(() =>
+                readline.question(
+                    '\x1b[38;5;79mContinue ? (y/n) : >',
+                    (reponse) => {
+                        if (reponse == 'y') {
+                            return app();
+                        } else {
+                            readline.close();
+                        }
+                    }
+                )
+            )
+            .catch((err) => console.log(`Erreur : ${err}`));
+    });
+}
+app();
